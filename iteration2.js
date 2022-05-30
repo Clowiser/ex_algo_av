3422ac31dd50e5d5d8140190ff131345017b7ba2
Clowiser <jessica.rigaud@le-campus-numerique.fr>
2022-05-30 16:49:02 +0200

    update algo de tri

--- iteration2.js
+++ iteration2.js
@@ -4,7 +4,7 @@
 console.log([3,9,7,1,6,2,8,4,5]);
 
 const tri = (list) => {
-    debugger;
+    //debugger;
     for(let i = 0; i < list.length; i++){
         //stocker l'index de l'élément minimum
         let min = i;
@@ -24,8 +24,19 @@ let tab1 = [3,9,7,1,6,2,8,4,5];
 console.log(tri(tab1));
 
 
-const tribulle = (tableau) => {
-let passage = 0;
-for(let i = 0; i<tableau.length; i++){
+function insertionSort(lst) {
+    let size = lst.length;
+    for (let i = 1; i < size; i++) {
+        let current = lst[i];
+        let j = i-1;
+        while ((j > -1) && (current < lst[j])) {
+            lst[j+1] = lst[j];
+            j--;
+        }
+        lst[j+1] = current;
+    }
+    return lst;
 }
-}
+
+let listeChiffre = [3,9,7,1,6,2,8,4,5];
+console.log(insertionSort(listeChiffre));
