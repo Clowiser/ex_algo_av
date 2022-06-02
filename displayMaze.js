8fc581a567db26198126e64dcc8eef8f71523b37
Clowiser <jessica.rigaud@le-campus-numerique.fr>
2022-06-02 16:41:52 +0200

    update labyrinthe work

--- displayMaze.js
+++ displayMaze.js
@@ -3,7 +3,9 @@ console.log(mazes);
 
 let mazeLength = mazes["3"]["ex-0"].length;
 
-function labyrinthe(){
+
+function displayMaze(){
+    debugger;
     for (let i = 0; i < mazeLength; i++){
         let wrapper = document.getElementById('wrapper');
         // affichage sur le html grâce getElementByID
@@ -13,8 +15,23 @@ function labyrinthe(){
         // ajout du css sur la class case par rapport à la div déclaré newCase
         wrapper.appendChild(newCase);
         // ajoute le nombre de noeuds (ici les div) créé par rapport à la taille du labyrinthe à l'élément wrapper
+
+        let mazeWall = mazes["3"]["ex-0"][i].walls[i];
+        console.log(mazeWall);
+
+        if(mazeWall === false){
+           newCase.style.borderRight = "none";
+        }
+        if(mazeWall === false){
+        newCase.style.borderLeft = "none";
+        }
+        if(mazeWall === false){
+             newCase.style.borderTop = "none";
+        }
+        if(mazeWall === false){
+             newCase.style.borderBottom = "none";
+        }
     }
 }
 
-labyrinthe();
-
+displayMaze();
